c112d55f04405c213b38919c9f20e4789902596e
Hoff Silva <hoff.henry@gmail.com>
2020-06-26 22:30:14 -0300

    First commit with initial project

---
+++ CCGitApi/CCGitApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CCGitApi
+//
+//  Created by Hoff Henry Pereira da Silva on 26/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
